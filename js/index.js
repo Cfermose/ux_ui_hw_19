0aecccd107f83e2c20d737a418f855d62efc059c
Cfermose <carolinefermoselle@gmail.com>
2021-03-19 19:48:10 -0400

    update

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$('#viewresume').on('click', function() {
+
+    $("#viewresume").css("color","blue");
+
+});
+
+// Step 1: Hide a div
+$(".heading").on("click", function(){
+    $("flexContainer").slideToggle();
+ 
+});
